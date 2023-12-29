994ed3aa61b4cd86df96077ed8ac3902f32cbeca
Michelle Amina Okumu <michelleaokumu@gmail.com>
2023-12-29 16:50:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+//let book; 
+
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp=>resp.json())
+  .then(data=> {
+    renderBooks(data); 
+  }); 
 }
+fetchBooks()
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -12,6 +20,8 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', fetchBooks); 
+
+
+
+
